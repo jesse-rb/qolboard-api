cc39d6d97a1399720220c7bf030c6b8262ab1ce5
jesse-rb <jesse.reynekebarnard@gmail.com>
2026-07-20 17:59:49 +1200

    (fix) remove Debug code

--- middleware/response/response_middleware.go
+++ middleware/response/response_middleware.go
@@ -11,7 +11,7 @@ func Run(c *gin.Context) {
 	c.Next()
 
 	logging.LogDebug("[middleware]", "[response]", nil)
-	if !c.Writer.Written() || true {
+	if !c.Writer.Written() {
 		response_service.Response(c)
 	}
 }
